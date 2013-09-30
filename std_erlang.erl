8548bfbd2c9ace7eec572881ec999d0fce0e0269
Nick Smallbone <nick.smallbone@gmail.com>
2013-09-30 18:22:14 +0200

    Remove some old guards from std_erlang.

--- std_erlang.erl
+++ std_erlang.erl
@@ -1,14 +1,7 @@
 -module(std_erlang).
 -compile(export_all).
 
--compile({symbolic, is_integer/1}).
-is_integer(X) ->
-    {guard, is_integer, [X]}.
-
--compile({symbolic, '>='/2}).
-'>='(X, Y) ->
-    {guard, '>=', [X, Y]}.
-
--compile({symbolic, 'and'/2}).
-'and'(X, Y) ->
-    {guard, 'and', [X, Y]}.
+'and'(false, X) when is_boolean(X) ->
+    false;
+'and'(true, X) when is_boolean(X) ->
+    X.
