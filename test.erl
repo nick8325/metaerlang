b9a9fee9c6a431a1a41dfeb4e00a1e07784be85a
Nick Smallbone <nick.smallbone@gmail.com>
2013-09-30 14:29:46 +0200

    Make the test module more taxing.

--- test.erl
+++ test.erl
@@ -7,3 +7,35 @@ member(X, [X|_]) ->
     true;
 member(X, [_|Xs]) ->
     member(X, Xs).
+
+f(Xs) ->
+    concat([Zs || Ys <- inits(Xs), Zs <- tails(Ys)]).
+
+inits(Xs) ->
+    map(fun reverse/1, tails(Xs)).
+
+map(_F, []) ->
+    [];
+map(F, [X|Xs]) ->
+    [F(X)|map(F, Xs)].
+
+concat(Xs) ->
+    concat([], Xs).
+concat([], []) ->
+    [];
+concat([], [Xs|Xss]) ->
+    concat(Xs, Xss);
+concat([X|Xs], Xss) ->
+    [X|concat(Xs, Xss)].
+
+reverse(Xs) ->
+    reverse(Xs, []).
+reverse([], Ys) ->
+    Ys;
+reverse([X|Xs], Ys) ->
+    reverse(Xs, [X|Ys]).
+
+tails([]) ->
+    [[]];
+tails(Xs=[_|Ys]) ->
+   [Xs|tails(Ys)].
