890e4b230fe1df1b7fdd7a9b7e3f1533af0225ea
Nick Smallbone <nick.smallbone@gmail.com>
2013-09-30 17:24:27 +0200

    Add more generator tests.

--- test_gen.erl
+++ test_gen.erl
@@ -2,15 +2,25 @@
 -compile(export_all).
 -include_lib("eqc/include/eqc.hrl").
 
-atoms() ->
+atom() ->
     elements([a,b,c,d,e,f,g,h]).
 
 unique_list() ->
     unique_list([]).
 unique_list(Xs) ->
     oneof([[],
-           ?LET(X, ?SUCHTHAT(Y, atoms(), not member(Y, Xs)),
+           ?LET(X, ?SUCHTHAT(Y, atom(), not member(Y, Xs)),
                [X|unique_list([X|Xs])])]).
+
+list_of(Gen) ->
+    ?LAZY(
+    oneof([[], [Gen|list_of(Gen)]])).
+
+atom_list() ->
+    list_of(atom()).
+
+one_atom_list() ->
+    ?LET(X, atom(), list_of(X)).
 
 member(_, []) ->
     false;
