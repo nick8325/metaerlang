4a769f30198262db4114fc81fe3a16ff8ec41234
Nick Smallbone <nick.smallbone@gmail.com>
2013-09-30 16:41:58 +0200

    Generate probabilities too :)

--- eqc_gen.pl
+++ eqc_gen.pl
@@ -10,25 +10,39 @@ suchthat(suchthat(Gen, P), Gen, P).
 oneof(oneof(Xs), Xs).
 elements(elements(Xs), Xs).
 
-in(X, bind(Gen, F)) :-
+in(X, Gen) :-
+    in(X, Gen, _).
+
+in(X, bind(Gen, F), P1*P2) :-
     !,
-    in(Y, Gen),
+    in(Y, Gen, P1),
     apply_fun(eqc_gen, F, Gen1, [Y]),
-    in(X, Gen1).
-in(X, suchthat(Gen, P)) :-
+    in(X, Gen1, P2).
+in(X, suchthat(Gen, Pred), P) :-
     !,
-    apply_fun(eqc_gen, P, true, [X]),
-    in(X, Gen).
-in(X, oneof(Xs)) :-
+    apply_fun(eqc_gen, Pred, true, [X]),
+    in(X, Gen, P).
+in(X, oneof(Xs), P/N) :-
     !,
     member(Gen, Xs),
-    in(X, Gen).
-in(X, elements(Xs)) :-
+    length(Xs, N),
+    in(X, Gen, P).
+in(X, elements(Xs), 1/N) :-
     !,
-    member(X, Xs).
-in(X, [Y|Ys]) :-
+    member(X, Xs),
+    length(Xs, N).
+in(X, [Y|Ys], P1*P2) :-
     !,
     X = [Z|Zs],
-    in(Z, Y),
-    in(Zs, Ys).
-in(X, X).
+    in(Z, Y, P1),
+    in(Zs, Ys, P2).
+in(X, X, 1).
+
+prob(X, Gen, P) :-
+    findall(P, in(X, Gen, P), Probs),
+    sum(Probs, P).
+sum([], 0).
+sum([P|Ps], Total) :-
+    N is P,
+    sum(Ps, Rest),
+    Total is N + Rest.
